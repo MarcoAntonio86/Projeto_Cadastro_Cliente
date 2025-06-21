50fbbc18da9d9c1a16d88a10f240fb58f3f64ed9
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-06-21 17:55:05 -0300

    Criação pacote exceptions e suas classes

---
+++ src/main/java/com/devsuperior/Projeto_Cadastro_Cliente/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.devsuperior.Projeto_Cadastro_Cliente.controllers.exceptions;
+
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Recurso não encontrado");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Erro de validação");
+        err.setMessage("Um ou mais campos estão inválidos");
+        err.setPath(request.getRequestURI());
+
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        for (FieldError f : fieldErrors) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
